fae90564f18b626fb0d9e7de2bfabfe868843b8e
Ognjen <radisicognjen@gmail.com>
2021-04-23 18:20:35 +0200

    descriptions in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -10,6 +10,25 @@ import Numbers from "./Numbers";
 import Operations from "./Operations";
 
 const Game = () => {
+	const [fastRenderNumber, setFastRenderNumber] = useState([
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+	]);
+
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
+
+	const getNextNumber = () => {};
+
 	return (
 		<Container
 			maxWidth="sm"
@@ -20,18 +39,32 @@ const Game = () => {
 				borderRadius: "10px",
 				paddingTop: "5px",
 			}}>
+			{/* TITLE MyNumber */}
 			<Typography variant="h4" style={{ margin: "10px 0" }}>
 				Moj Broj
 			</Typography>
-			<MyButton color="blue">START</MyButton>
+
+			{/* Start the game, and stop each number in place */}
+			<MyButton color="blue" onCLick={getNextNumber}>
+				START
+			</MyButton>
+
+			{/* timer that starts after the last digit loaded, 50 seconds timer */}
 			<LinearWithValueLabel />
+
+			{/* 3 individual digits at the top that make target number, 4 single digits, one 'medium' number, one 'large' number */}
 			<Numbers />
+
+			{/* divide lower field in two parts, on the left user input, on the right mathematical operations */}
 			<Grid
 				container
 				spacing={1}
 				justify="space-around"
 				style={{ margin: "10px 0" }}>
+				{/* Left part with user input */}
 				<Grid item>input</Grid>
+
+				{/* Right part with mathematical operations */}
 				<Grid item>
 					<Operations />
 				</Grid>
