bf86045efa578d47bfc3bf0c5659054ae2e22b4a
Ognjen <radisicognjen@gmail.com>
2021-04-26 15:30:11 +0200

    basic delete functionality and styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,39 @@
 import React from "react";
+import "./game.css";
 import { GlobalContext } from "../AppContext";
+import { Button } from "@material-ui/core";
 
-const Input = (props) => {
-	const { resultField } = GlobalContext();
+const Input = () => {
+	const { resultField, setResultField } = GlobalContext();
+
+	const deleteCharacter = () => {
+		const lastChar = resultField.charAt(resultField.length - 1);
+		if (resultField === "") {
+			return;
+		} else if (isNaN(parseInt(lastChar))) {
+			const newResult = resultField.slice(0, -1);
+			setResultField(newResult);
+		} else {
+			const newResult = resultField.slice(0, -1);
+			setResultField(newResult);
+		}
+	};
 
 	return (
 		<>
-			<div className="result-field">
-				{props.resultField}
-				<h2>{resultField}</h2>
-				<button>delete</button>
-				<button>confirm</button>
+			<div className="result-section">
+				<div className="result-field">{resultField}</div>
+				<div className="button-section">
+					<Button
+						color="secondary"
+						variant="contained"
+						onClick={deleteCharacter}>
+						DELETE
+					</Button>
+					<Button color="primary" variant="contained">
+						CONFIRM
+					</Button>
+				</div>
 			</div>
 		</>
 	);
