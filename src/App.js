2754e562be64d5326db451287a7b49842f62b6a1
Ognjen <radisicognjen@gmail.com>
2021-04-30 16:06:06 +0200

    added links and minor changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Game from "./components/Game";
 import "fontsource-roboto";
 import { AppProvider } from "./AppContext";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import LinkToRepo from "./components/LinkToRepo";
 
 function App() {
 	const handleFullscreen = useFullScreenHandle();
@@ -12,6 +13,7 @@ function App() {
 			<FullScreen handle={handleFullscreen}>
 				<div className="App">
 					<Game handleFullscreen={handleFullscreen} />
+					<LinkToRepo />
 				</div>
 			</FullScreen>
 		</AppProvider>
---
+++ src/components/LinkToRepo.js
@@ -0,0 +1,41 @@
+import React from "react";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { Button, Container } from "@material-ui/core";
+
+const LinkToRepo = () => {
+	return (
+		<Container
+			maxWidth="sm"
+			style={{
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+			}}>
+			<h2 style={{ color: "#050A30" }}>
+				by{" "}
+				<a
+					style={{ color: "inherit", textDecoration: "none" }}
+					href="https://github.com/Ognjen-Radisic">
+					Ognjen Radisic
+				</a>
+			</h2>
+			<Button
+				variant="contained"
+				style={{
+					background: "green",
+					height: "30px",
+					color: "#000C66",
+					backgroundColor: "#7EC8E3",
+					fontWeight: "1000",
+				}}
+				onClick={() => window.open("https://www.google.com/", "_blank")}>
+				<FavoriteIcon
+					style={{ fontSize: "15px", marginRight: "5px", marginBottom: "2px" }}
+				/>
+				Code
+			</Button>
+		</Container>
+	);
+};
+
+export default LinkToRepo;
