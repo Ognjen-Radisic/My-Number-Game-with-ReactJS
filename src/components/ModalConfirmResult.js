aaba87d87290a08544733ceeb609daaf4b4e8a25
Ognjen <radisicognjen@gmail.com>
2021-05-01 14:24:54 +0200

    confirmResultModal styling

--- src/components/ModalConfirmResult.js
+++ src/components/ModalConfirmResult.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Grid } from "@material-ui/core";
 import { animated, config, useSpring } from "@react-spring/web";
 import { GlobalContext } from "../AppContext";
+import "./modalHowTo.css";
 
 //import images
 import golden_trophy from "../resources/trophy.svg";
@@ -24,26 +25,25 @@ const ModalConfirmResult = () => {
 			...config.wobbly,
 		},
 		opacity: resultModalVisible ? 1 : 0,
-		transform: resultModalVisible ? `translateY(0%)` : `translateY(-100%)`,
+		transform: resultModalVisible ? `translateX(0%)` : `translateX(-100%)`,
 	});
 
 	return (
 		<>
 			{resultModalVisible ? (
 				<div className="background">
-					<animated.div style={animation} className="modal-wrapper">
-						<Grid container direction="column" className="grid-container">
-							<Grid item>
-								<h3>RESULT</h3>
-								<h4>Goal number: {targetNum}</h4>
-							</Grid>
-							<Grid item>
-								<h4>Your result:</h4>
-								<h4>
-									{resultField} = {eval(resultField)}
-								</h4>
-							</Grid>
-							<Grid item>
+					<animated.div
+						style={{ ...animation, width: "340px" }}
+						className="modal-wrapper">
+						<Grid
+							container
+							direction="column"
+							className="grid-container"
+							justify="space-between"
+							style={{ backgroundColor: "#C3CEDA" }}>
+							<Grid
+								item
+								style={{ padding: "30px", backgroundColor: "#0C4160" }}>
 								{/* First changable thing in modal, 3 variants */}
 								<img
 									src={
@@ -59,16 +59,56 @@ const ModalConfirmResult = () => {
 							</Grid>
 							<Grid item>
 								{/* Second changable thing in modal, 3 variants */}
-								<h4>
-									{isResultCorrect
-										? progress <= 50
-											? "GOLDEN TROPHY! Well done, more then half of your time remained."
-											: "SILVER TROPHY! Well done you solved it, try to do it faster next time and earn golden trophy."
-										: "You made a mistake, this time.."}
-								</h4>
+
+								{isResultCorrect ? (
+									progress <= 50 ? (
+										<div>
+											<h3>GOLDEN TROPHY!</h3>
+											<h4>
+												Well done you are FAST, more then half of your time
+												remained.
+											</h4>
+										</div>
+									) : (
+										<div>
+											<h3>SILVER TROPHY!</h3>
+											<h4>
+												Well done you solved it, try to do it faster next time
+												and earn golden trophy.
+											</h4>
+										</div>
+									)
+								) : (
+									<h3>You made a mistake, this time..</h3>
+								)}
 							</Grid>
-							<Grid item className="bottom-buttons">
-								<Button onClick={() => everythingDefault()}>Play Again</Button>
+							<Grid item>
+								<div>
+									{"Number to find:     "}
+									{targetNum}
+								</div>
+							</Grid>
+							<Grid item>
+								<div>Your result</div>
+								<div>
+									{resultField} = {eval(resultField)}
+								</div>
+							</Grid>
+
+							<Grid
+								item
+								className="bottom-buttons"
+								style={{ marginBottom: "10px", justifyContent: "center" }}>
+								<Button
+									variant="outlined"
+									onClick={() => everythingDefault()}
+									style={{
+										backgroundColor: "#0C4160",
+										color: "white",
+										fontWeight: "800",
+									}}>
+									Play Again
+								</Button>
 							</Grid>
 						</Grid>
 					</animated.div>
